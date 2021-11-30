4266da121e59cb2e4f48ac21f7f364522382bfd7
Ola <ol.holovina@gmail.com>
2021-11-30 10:16:42 -0500

    condition for last roll add

--- js/script.js
+++ js/script.js
@@ -2,13 +2,23 @@ const getRandomDiceRoll = function(sides=6) {
   return Math.floor( Math.random() * sides ) + 1
 }
 
+let lastRoll = 0;
 const rollTheDice = () => {
   const diceRoll = getRandomDiceRoll();
-  document.querySelector('.message b').textContent = diceRoll;
+
   const image = document.querySelector('.dice img');
   image.setAttribute('src', `img/dice${diceRoll}.svg`);
   image.setAttribute('alt', `Rolled ${diceRoll}`);
+
+  const messageEl = document.querySelector('.message b');
+  if (lastRoll === diceRoll) {
+    messageEl.textContent = `${diceRoll}, AGAIN!`;
+  } else {
+    messageEl.textContent = diceRoll;
+  }
+
+  lastRoll = diceRoll;
 }
 
-document.getElementById('roll-button').addEventListener('click', rollTheDice);
+document.querySelector('#roll-button').addEventListener('click', rollTheDice);
 window.addEventListener('load', rollTheDice);
