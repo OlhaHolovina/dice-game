ba28981604e72e602386fba41f84c3015a76c4d9
Ola <ol.holovina@gmail.com>
2021-11-30 11:59:34 -0500

    getRollInWords add

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@ const getRandomDiceRoll = function(sides=6) {
   return Math.floor( Math.random() * sides ) + 1
 }
 
+const getRollInWords = (diceRoll) => {
+  if (diceRoll === 1) {
+    return `one`;
+  } else if (diceRoll === 2) {
+    return `two`;
+  } else if (diceRoll === 3) {
+    return `three`;
+  } else if (diceRoll === 4) {
+    return `four`;
+  } else if (diceRoll === 5) {
+    return `five`;
+  } else if (diceRoll === 6) {
+    return `six`;
+  } else {
+    console.error(`Woops! Something went wrong`);
+    return diceRoll;
+  }
+}
+
 let lastRoll = 0;
 const rollTheDice = () => {
   const diceRoll = getRandomDiceRoll();
@@ -11,10 +30,11 @@ const rollTheDice = () => {
   image.setAttribute('alt', `Rolled ${diceRoll}`);
 
   const messageEl = document.querySelector('.message b');
+  const rollInWords = getRollInWords(diceRoll);
   if (lastRoll === diceRoll) {
-    messageEl.textContent = `${diceRoll}, AGAIN!`;
+    messageEl.textContent = `${rollInWords}, AGAIN!`;
   } else {
-    messageEl.textContent = diceRoll;
+    messageEl.textContent = rollInWords;
   }
 
   lastRoll = diceRoll;
